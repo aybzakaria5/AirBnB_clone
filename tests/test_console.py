581ea4b6b4875da69afae7316f630bdb572a0ee8
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-11-12 19:10:27 +0100

    fixing pycodestyle4

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,6 +2,7 @@
 """a inittest for the consol module
 """
 import unittest
+import re
 from console import HBNBCommand
 from models import FileStorage
 from models import storage
@@ -15,9 +16,9 @@ class TestHBNBCommand(unittest.TestCase):
     # -------------------------------------------------------------------
     # Create an instance of an existing class and verify that it was saved
     def test_create_existing_class_saved(self):
-        '''
+        """
         Test that do_create saves an instance of an existing class
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("create BaseModel")
             output = f.getvalue().strip()
@@ -26,9 +27,9 @@ class TestHBNBCommand(unittest.TestCase):
 
     # Create an instance of an existing class and verify that it has an id
     def test_create_existing_class_has_id(self):
-        '''
+        """
         Test that do_create assigns an id to an instance of an existing class
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("create BaseModel")
             output = f.getvalue().strip()
@@ -39,10 +40,10 @@ class TestHBNBCommand(unittest.TestCase):
         # Create an instance of an existing class with
         # arguments and verify that it was saved
     def test_create_existing_class_with_args_saved(self):
-        '''
+        """
         Test that do_create saves an instance of
         an existing class with arguments
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd('create BaseModel')
             output = f.getvalue().strip()
@@ -58,17 +59,65 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertTrue(output)
             obj_id = output
             self.assertIn(f"BaseModel.{obj_id}", storage.all().keys())
+    
+    # Test that do_create does not save an instance of a non-existing class
+    def test_create_non_existing_class_not_saved(self):
+        """
+        Test that do_create does not save an instance of a non-existing class
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("create NonExistingClass")
+            output = f.getvalue().strip()
+            self.assertEqual("** class doesn't exist **", output)
+            self.assertNotIn("NonExistingClass", storage.all().keys())
+    
+    # Create an instance of a non-existing class and verify
+    # that it returns an error message
+    def test_create_non_existing_class_returns_error_message(self):
+        """
+        Test that do_create returns an error message when creating an instance of a non-existing class
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("create NonExistingClass")
+            output = f.getvalue().strip()
+            self.assertEqual("** class doesn't exist **", output)
 
+
+    # Test that do_create does not save an instance of an existing class with invalid arguments
+    def test_create_existing_class_invalid_args_not_saved(self):
+        """
+        Test that do_create does not save an instance of an existing class with invalid arguments
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd('create BaseModel name=test number="invalid"')
+            output = f.getvalue().strip()
+            self.assertTrue(re.match(r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$', output))
+            self.assertNotIn("BaseModel", storage.all().keys())
+
+        # Test that do_create assigns a created_at attribute to an instance of an existing class
+    def test_create_existing_class_has_created_at(self):
+        """
+        Test that do_create assigns a created_at attribute to an instance of an existing class
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(output)
+            obj_id = output
+            key = f"BaseModel.{obj_id}"
+            self.assertIn(key, storage.all().keys())
+            self.assertIn("created_at", storage.all()[key].to_dict())
+    
     # tests for the destroy methode
     # ---------------------------------------------------------
     # Deletes an instance based on the class name
     # and id when no instance id is provided.
 
     def test_destroy_BaseModel_no_instance_id(self):
-        '''
+        """
         Test that do_destroy deletes an instance based on the class name and id
         when no instance id is provided
-        '''
+        """
         from unittest.mock import patch
         from io import StringIO
 
@@ -79,9 +128,9 @@ class TestHBNBCommand(unittest.TestCase):
 
     # Saves the change into the JSON file.
     def test_destroy_BaseModel_save_change(self):
-        '''
+        """
         Test that do_destroy saves the change into the JSON file
-        '''
+        """
         storage = FileStorage()
         storage.reload()
         with patch("sys.stdout", new=StringIO()) as f:
@@ -92,27 +141,27 @@ class TestHBNBCommand(unittest.TestCase):
 
     # The test checks if the do_destroy method returns None.
     def test_destroy_BaseModel_returns_none(self):
-        '''
+        """
         Test that do_destroy returns None
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             result = HBNBCommand().onecmd("destroy BaseModel")
             self.assertIsNone(result)
 
     # Test that do_destroy prints nothing.
     def test_destroy_BaseModel_prints_nothing(self):
-        '''
+        """
         Test that do_destroy prints nothing when no instance id given
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("destroy BaseModel")
             self.assertEqual("** instance id missing **", f.getvalue().strip())
 
     # Does not raise any exceptions.
     def test_destroy_BaseModel_no_exceptions(self):
-        '''
+        """
         Test that do_destroy does not raise any exceptions
-        '''
+        """
         try:
             HBNBCommand().onecmd("destroy BaseModel")
         except Exception as e:
@@ -121,10 +170,10 @@ class TestHBNBCommand(unittest.TestCase):
     # If no arguments are passed, prints "**
     # class name missing **".
     def test_destroy_no_class_name(self):
-        '''
+        """
         Test that do_destroy prints "** class name missing **"
         if no arguments are passed
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("destroy")
             expected_output = "** class name missing **"
@@ -133,10 +182,10 @@ class TestHBNBCommand(unittest.TestCase):
     # class doesn't exist **".
 
     def test_destroy_invalid_class_name_(self):
-        '''
+        """
         Test that do_destroy prints "** class doesn't exist **"
         if the class name does not exist
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("destroy InvalidClass")
             expected_output = "** class doesn't exist **"
@@ -144,10 +193,10 @@ class TestHBNBCommand(unittest.TestCase):
 
         # If the instance id is missing, prints "** instance id missing **".
     def test_destroy_BaseModel_missing_instance_id_(self):
-        '''
+        """
         Test that do_destroy prints "** instance id missing **"
         if the instance id is missing
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("destroy BaseModel")
             expected_output = "** instance id missing **"
@@ -155,10 +204,10 @@ class TestHBNBCommand(unittest.TestCase):
 
         # If no instance is found, prints "** no instance found **".
     def test_destroy_BaseModel_no_instance_found_(self):
-        '''
+        """
         Test that do_destroy prints "** no instance found **"
         if no instance is found
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("destroy BaseModel 12345")
             expected_output = "** no instance found **"
@@ -167,10 +216,10 @@ class TestHBNBCommand(unittest.TestCase):
         # If the instance is found,
         # deletes it from the storage.
     def test_destroy_BaseModel_instance_found_deletes_from_storage(self):
-        '''
+        """
         Test that do_destroy deletes the
         instance from the storage if it is found
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("create BaseModel")
             obj_id = f.getvalue().strip()
@@ -180,10 +229,10 @@ class TestHBNBCommand(unittest.TestCase):
     # If the instance is found,
     # saves the change into the JSON file.
     def test_destroy_BaseModel_instance_found_saves_change(self):
-        '''
+        """
         Test that do_destroy saves the change
         into the JSON file if the instance is found
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("create BaseModel")
             obj_id = f.getvalue().strip()
@@ -194,10 +243,10 @@ class TestHBNBCommand(unittest.TestCase):
         # Deletes an instance when given
         # a valid class name and id with numbers.
     def test_destroy_valid_class_name_and_id_with_numbers(self):
-        '''
+        """
         Test that do_destroy deletes an instance when
         given a valid class name and id with numbers
-        '''
+        """
         with patch("sys.stdout", new=StringIO()) as f:
             HBNBCommand().onecmd("create BaseModel")
             obj_id = f.getvalue().strip()
@@ -208,10 +257,10 @@ class TestHBNBCommand(unittest.TestCase):
     # Deletes an instance when given a valid
     # class name and id with special characters.
     def test_destroy_valid_class_name_and_id_with_special_characters(self):
-        '''
+        """
         Test that do_destroy deletes an instance when given
         a valid class name and id with special characters
-        '''
+        """
         storage = FileStorage()
         storage.reload()
         with patch("sys.stdout", new=StringIO()) as f:
